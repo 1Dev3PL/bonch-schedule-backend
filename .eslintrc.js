a44ada49de410f3c7dc92f6a6a45442da165a788
tige21 <eeper03@mail.ru>
2023-05-09 13:00:38 +0300

    first commit in my own branch

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: 'script'
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
@@ -21,5 +21,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "prettier/prettier": ["error",{
+      "endOfLine": "auto"}
+    ]
   },
 };
